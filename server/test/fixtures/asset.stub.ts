f6847fbbd6dfcc437e82687334265b872426d1bc
mertalev <101130780+mertalev@users.noreply.github.com>
2025-04-08 20:41:54 -0400

    remove face, person and face search entities
    update tests and mappers
    
    check if face relation exists

--- server/test/fixtures/asset.stub.ts
+++ server/test/fixtures/asset.stub.ts
@@ -7,7 +7,7 @@ import { authStub } from 'test/fixtures/auth.stub';
 import { fileStub } from 'test/fixtures/file.stub';
 import { userStub } from 'test/fixtures/user.stub';
 
-const previewFile: AssetFile = {
+export const previewFile: AssetFile = {
   id: 'file-1',
   type: AssetFileType.PREVIEW,
   path: '/uploads/user-id/thumbs/path.jpg',
@@ -25,7 +25,7 @@ const fullsizeFile: AssetFile = {
   path: '/uploads/user-id/fullsize/path.webp',
 };
 
-const files: AssetFile[] = [fullsizeFile, previewFile, thumbnailFile];
+export const files: AssetFile[] = [fullsizeFile, previewFile, thumbnailFile];
 
 export const stackStub = (stackId: string, assets: AssetEntity[]): StackEntity => {
   return {
